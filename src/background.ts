debc1ecaf8d2afcfa1cb7a91ff55add372e5a532
Daniel Gilchrist <daniel@workforce.com>
2021-12-12 20:27:47 +0000

    Fix up typescript errors + remove unnecessary code

--- src/background.ts
+++ src/background.ts
@@ -1,16 +1,18 @@
-document.getElementById('video-js_html5_api').addEventListener('ended', () {
-  const nextLink = document.querySelector('a[rel]');
-}, false);
-
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status == 'complete' && tab.active) {
-    const videoFrame = document.querySelector("#frameNewAnimeuploads0")
-    const video = videoFrame.contentDocument.querySelector("video#video-js_html5_api")
-    const videoRoot = video.parentElement
-    const nextLink = document.querySelector("a[rel='next']")
+    const videoFrame = (document.querySelector("#frameNewAnimeuploads0") as HTMLIFrameElement)
+    if (videoFrame == null) return
+
+    const video = (videoFrame?.contentDocument?.querySelector("video#video-js_html5_api") as HTMLVideoElement)
+    if (video == null) return
+
+    const videoRoot = (video.parentElement as HTMLDivElement)
+    if (videoRoot == null) return
+
+    const nextLink = (document.querySelector("a[rel='next']") as HTMLAnchorElement)
 
     video.play().then(() => {
-      const fullscreenButton = videoRoot.querySelector("button.vjs-fullscreen-control")
+      const fullscreenButton = (videoRoot.querySelector("button.vjs-fullscreen-control") as HTMLButtonElement)
       if (fullscreenButton == null) {
         return
       }
