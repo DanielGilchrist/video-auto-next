44f8585a7e24cb27070fae976d919b0a4f4e6f2e
Daniel Gilchrist <daniel@workforce.com>
2021-12-13 00:15:11 +0000

    This stuff really shouldn't be in background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,18 +1,5 @@
-import { findNextLink, findVideoElement } from "./helpers/wcostream"
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   if (changeInfo.status == 'complete' && tab.active) {
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status == 'complete' && tab.active) {
-    const video = findVideoElement()
-    if (video == null) return
-
-    const nextLink = findNextLink()
-
-    video.play().then(() => video.requestFullscreen());
-
-    if (nextLink) {
-      video.addEventListener('ended', () => {
-        window.location.href = nextLink.href
-      }, false);
-    }
-  }
-})
+//   }
+// })
--- src/helpers/wcostream/index.ts
+++ src/helpers/wcostream/index.ts
@@ -1,10 +1,31 @@
-export function findNextLink (): HTMLAnchorElement | null {
+function findNextLink (): HTMLAnchorElement | null {
   return document.querySelector<HTMLAnchorElement>("a[rel='next']")
 }
 
-export function findVideoElement (): HTMLVideoElement | null | undefined {
+function findVideoElement (): HTMLVideoElement | null | undefined {
   const videoFrame = document.querySelector<HTMLIFrameElement>("#frameNewAnimeuploads0")
   if (videoFrame == null) return
 
   return videoFrame?.contentDocument?.querySelector<HTMLVideoElement>("video#video-js_html5_api")
+}
+
+function withEventListener(video: HTMLVideoElement , callback: () => void): void {
+  const nextLink = findNextLink()
+
+  callback()
+
+  if (nextLink) {
+    video.addEventListener('ended', () => {
+      window.location.href = nextLink.href
+    }, false);
+  }
+}
+
+export function playVideoAndSetListener () {
+  const video = findVideoElement()
+  if (video == null) return
+
+  withEventListener(video, () => {
+    video.play().then(() => video.requestFullscreen())
+  })
 }
